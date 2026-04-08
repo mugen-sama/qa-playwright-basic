3496866fc709a18b6fb3f0ec2249b95cc8cea0e7
mugen-sama <sulton.mukarobin@gmail.com>
2026-04-08 14:41:55 +0700

    update login test & remove old test file

--- tests/login.demo.spec.ts
+++ tests/login.demo.spec.ts
@@ -8,5 +8,18 @@ test('Login Test', async ({ page }) => {
     const passwordInput: Locator = page.locator('#password');
     const loginButton: Locator = page.locator('#login-button');
 
+    // Perform login
+    await usernameInput.fill('standard_user');
+    await passwordInput.fill('secret_sauce');
+    await loginButton.click();  
+
+    // Verify successful login by checking for the presence of the inventory container
+    // const inventoryContainer: Locator = page.locator('.inventory_container');
+    // const inventoryContainer = page.locator('[data-test="inventory_container"]');
+    // await expect(inventoryContainer).toBeVisible(); 
+    // await expect(page.getByText('Products')).toBeVisible();
+    await expect(
+      page.locator('[data-test="title"]')
+    ).toHaveText('Products');
 
 })
